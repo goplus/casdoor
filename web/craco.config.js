4fb6d152dc50a00bb95bd85f09c2e579932ec256
ComfyFluffy <24245520+ComfyFluffy@users.noreply.github.com>
2024-09-18 10:47:00 +0800

    cleanup

--- web/craco.config.js
+++ web/craco.config.js
@@ -4,41 +4,41 @@ module.exports = {
   devServer: {
     proxy: {
       "/api": {
-        target: "https://goplus-casdoor.qiniu.io",
+        target: "http://localhost:8000",
         changeOrigin: true,
       },
       "/swagger": {
-        target: "https://goplus-casdoor.qiniu.io",
+        target: "http://localhost:8000",
         changeOrigin: true,
       },
       "/files": {
-        target: "https://goplus-casdoor.qiniu.io",
+        target: "http://localhost:8000",
         changeOrigin: true,
       },
       "/.well-known/openid-configuration": {
-        target: "https://goplus-casdoor.qiniu.io",
+        target: "http://localhost:8000",
         changeOrigin: true,
       },
       "/cas/serviceValidate": {
-        target: "https://goplus-casdoor.qiniu.io",
+        target: "http://localhost:8000",
         changeOrigin: true,
       },
       "/cas/proxyValidate": {
-        target: "https://goplus-casdoor.qiniu.io",
+        target: "http://localhost:8000",
         changeOrigin: true,
       },
       "/cas/proxy": {
-        target: "https://goplus-casdoor.qiniu.io",
+        target: "http://localhost:8000",
         changeOrigin: true,
       },
       "/cas/validate": {
-        target: "https://goplus-casdoor.qiniu.io",
+        target: "http://localhost:8000",
         changeOrigin: true,
       },
       "/scim": {
-        target: "https://goplus-casdoor.qiniu.io",
+        target: "http://localhost:8000",
         changeOrigin: true,
-      },
+      }
     },
   },
   plugins: [
@@ -47,10 +47,7 @@ module.exports = {
       options: {
         lessLoaderOptions: {
           lessOptions: {
-            modifyVars: {
-              "@primary-color": "rgb(89,54,213)",
-              "@border-radius-base": "5px",
-            },
+            modifyVars: {"@primary-color": "rgb(89,54,213)", "@border-radius-base": "5px"},
             javascriptEnabled: true,
           },
         },
@@ -59,16 +56,16 @@ module.exports = {
   ],
   webpack: {
     configure: {
-      // ignore webpack warnings by source-map-loader
+      // ignore webpack warnings by source-map-loader 
       // https://github.com/facebook/create-react-app/pull/11752#issuecomment-1345231546
       ignoreWarnings: [
         function ignoreSourcemapsloaderWarnings(warning) {
           return (
             warning.module &&
-            warning.module.resource.includes("node_modules") &&
+            warning.module.resource.includes('node_modules') &&
             warning.details &&
-            warning.details.includes("source-map-loader")
-          );
+            warning.details.includes('source-map-loader')
+          )
         },
       ],
       // use polyfill Buffer with Webpack 5
@@ -84,21 +81,21 @@ module.exports = {
           // "http": require.resolve("stream-http"),
           // "https": require.resolve("https-browserify"),
           // "assert": require.resolve("assert/"),
-          // buffer: require.resolve("buffer/"),
-          process: false,
-          util: false,
-          url: false,
-          zlib: false,
-          stream: false,
-          http: false,
-          https: false,
-          assert: false,
-          buffer: false,
-          crypto: false,
-          os: false,
-          fs: false,
+          "buffer": require.resolve('buffer/'),    
+          "process": false,
+          "util": false,
+          "url": false,
+          "zlib": false,
+          "stream": false,
+          "http": false,
+          "https": false,
+          "assert": false,
+          "buffer": false,
+          "crypto": false,
+          "os": false,
+          "fs": false,
         },
-      },
+      }
     },
-  },
+  }
 };
